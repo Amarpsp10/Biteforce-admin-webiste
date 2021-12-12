207304b4ba1002b317ff6aa2c154902ecfe83b38
Amar Preet Singh <amarpsp10@gmail.com>
2021-12-12 15:42:17 +0530

    Institution Register & Device Register Page Updated

--- src/pages/register/DeviceRegister.js
+++ src/pages/register/DeviceRegister.js
@@ -1,24 +1,58 @@
-import React, { useState } from 'react'
+import React, { useContext, useEffect, useState } from 'react'
 import { isMobile } from 'react-device-detect'
-import {RegisterHeader} from '../../components'
+import {RegisterHeader, SelectInput, QrScanner} from '../../components'
 import { TextInput, ButtonContained, FormError } from '../../components'
+import { getAllInstitutions, registerDevice } from '../../api'
+import { AuthContext } from '../../contexts'
+import { useHistory } from 'react-router-dom'
+
 export default function InstituteRegister() {
+    const { user } = useContext(AuthContext)
     const [institution, setInstitution] = useState('')
+    const [insOp, setInsOp] = useState([])
     const [deviceId, setDeviceId] = useState('')
-    const [devicePassword, setDevicePassword] = useState('')
     const [loading, setLoading] = useState(false)
     const [error ,setError] = useState('')
+    const history = useHistory()
+    useEffect(()=>{
+        getInstitutions()
+    },[])
 
-    const handleRegister = async() =>{
-        
+    const getInstitutions = async() =>{
+        const allIns = await getAllInstitutions()
+        const arr = []
+        allIns.docs.map((doc)=>{
+            arr.push({value:doc.data().name,label:doc.data().name})
+        })
+        setInsOp(arr)
     }
+    const handleRegister = async() =>{
+        if(!institution || !deviceId){
+            return setError('All fields are required !')
+        }
+        setLoading(true)
+        const register = await registerDevice({device_id: deviceId, institution_name: institution, registered_by: user.uid})
+        if(register===undefined){
+            console.log('successfully registered')
+            history.push('/home/dashboard')
+        }
+        setLoading(false)
+    }
+
     return (
         <div>
             <RegisterHeader title='Register Device'/>
             <div style={{display:'flex',flexDirection:'column',paddingInline:isMobile?'5%':'30%'}}>
-                <TextInput value={institution} type='text' containerStyle={{marginTop:50}} onChange={(e)=>setInstitution(e)} title='Institution Name' placeholder=''/>
+                <br/>
+                <text style={{color:'gray',fontSize:20,marginBottom:10}}>Institutions</text>
+                <SelectInput placeholder={'Select Institution'} value={institution} setValue={(selected)=>setInstitution(selected)} options={insOp}/>
                 <TextInput value={deviceId} type='text' onChange={(e)=>setDeviceId(e)} title='Device Id' placeholder=''/>
-                <TextInput value={devicePassword} type='text' containerStyle={{marginBottom:30}} onChange={(e)=>setDevicePassword(e)} title='Device Password' placeholder=''/>
+                <QrScanner
+                    fps={10}
+                    qrbox={250}
+                    disableFlip={false}
+                    qrCodeSuccessCallback={(decodedText, decodedResult)=>setDeviceId(decodedText)}
+                />
                 <FormError message={error}/><br/>
                 <ButtonContained onClick={()=>handleRegister()} loading={loading}>Register Device</ButtonContained>
             </div>
--- src/pages/register/InstituteRegister.js
+++ src/pages/register/InstituteRegister.js
@@ -1,8 +1,8 @@
-import React, { useContext, useState } from 'react'
+import React, { useContext, useState, useEffect } from 'react'
 import { isMobile } from 'react-device-detect'
 import {FormError, RegisterHeader} from '../../components'
-import { TextInput, ButtonContained } from '../../components'
-import { checkInstitution, registerInstitute } from '../../api'
+import { TextInput, ButtonContained, SelectInput } from '../../components'
+import { checkInstitution, registerInstitute, allCities } from '../../api'
 import { AuthContext } from '../../contexts'
 import { useHistory } from 'react-router-dom'
 
@@ -11,9 +11,23 @@ export default function DeviceRegister() {
     const [institution, setInstitution] = useState('')
     const [address, setAdress] = useState('')
     const [city, setCity] = useState('')
+    const [cityOp, setCityOp] = useState([])
     const [loading, setLoading] = useState(false)
     const [error, setError] = useState('')
     const history = useHistory()
+
+    useEffect(()=>{
+        GetCities()
+    },[])
+
+    const GetCities = async() =>{
+        const allCity = await allCities()
+        const arr = []
+        allCity.result.map((data)=>{
+            arr.push({value:data.ascii_name,label:data.ascii_name})
+        })
+        setCityOp(arr)
+    }
 
     const handleRegister = async() =>{
         setError('')
@@ -42,7 +56,8 @@ export default function DeviceRegister() {
             <div style={{display:'flex',flexDirection:'column',paddingInline:isMobile?'5%':'30%'}}>
                 <TextInput value={institution} type='text' containerStyle={{marginTop:50}} onChange={(e)=>setInstitution(e)} title='Institution Name' placeholder=''/>
                 <TextInput value={address} type='text' onChange={(e)=>setAdress(e)} title='Address of Institution' placeholder=''/>
-                <TextInput value={city} type='text' onChange={(e)=>setCity(e)} title='City' placeholder=''/>
+                <text style={{color:'gray',fontSize:20,marginBottom:10}}>City</text>
+                <SelectInput value={city} setValue={(selected)=>setCity(selected)} options={cityOp} placeholder={'Select City'}/><br/>
                 <FormError message={error}/><br/>
                 <ButtonContained loading={loading} onClick={()=>handleRegister()}>Register</ButtonContained>
             </div>
